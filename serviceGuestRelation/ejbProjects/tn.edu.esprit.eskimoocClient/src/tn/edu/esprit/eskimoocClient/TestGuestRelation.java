bacc81cfc94e17c4ea0cb22386a54bbd95157949
HipPa <nhaythem1988@gmail.com>
2014-04-05 15:10:06 +0100

    second git

---
+++ serviceGuestRelation/ejbProjects/tn.edu.esprit.eskimoocClient/src/tn/edu/esprit/eskimoocClient/TestGuestRelation.java
@@ -0,0 +1,32 @@
+package tn.edu.esprit.eskimoocClient;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import tn.edu.esprit.eskimooc.services.interfaces.GuestRelationRemote;
+
+public class TestGuestRelation {
+
+	public static void main(String[] args) { 
+		try {
+			//esplacement memoire où on peut abréque le proxy
+			Context context = new InitialContext();
+			
+/*service guest relation via interface romote */
+			
+			String JndiName = "ejb:/tn.edu.esprit.eskimooc/GuestRelation!"
+					+ GuestRelationRemote.class.getCanonicalName();
+	
+			/*jndi:il va pointer sur la racine*/ 
+			GuestRelationRemote guestRelationRemote = (GuestRelationRemote) context
+					.lookup(JndiName);
+
+					guestRelationRemote.SayAhla("haythem");			
+		
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ serviceGuestRelation/ejbProjects/tn.edu.esprit.eskimoocClient/src/tn/edu/esprit/eskimoocClient/junitTest/TestGuestRelationervices.java
@@ -0,0 +1,38 @@
+package tn.edu.esprit.eskimoocClient.junitTest;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import tn.edu.esprit.eskimooc.services.interfaces.GuestRelationRemote;
+
+public class TestGuestRelationervices {
+
+	private Context context;
+
+	private GuestRelationRemote guestRelationRemote;
+
+	@Before
+	public void init() {
+		try {
+			context = new InitialContext(); // situé dans la racine du l'arbre
+											// JNDI
+
+			String jndiName = "ejb:/tn.edu.esprit.eskimooc/GesutRelation!"+ GuestRelationRemote.class.getCanonicalName;
+			guestRelationRemote = (GuestRelationRemote) context
+					.lookup(jndiName);
+
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+   @Test
+   public void testSayAhla(){
+   return Assert.assertEquals("ahla: haythem ");
+   }
+}
